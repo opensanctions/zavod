7b29cbdd56366dee35fbf9236ec48b0a6d9cf0a5
Friedrich Lindenberg <friedrich@pudo.org>
2022-06-11 20:44:36 +0200

    add comment

--- zavod/parse/xml.py
+++ zavod/parse/xml.py
@@ -2,7 +2,12 @@ from lxml import etree
 
 
 def remove_namespace(el: etree._Element) -> etree._Element:
-    """Remove all namespace details from the given XML document."""
+    """Remove namespace in the passed XML/HTML document in place and
+    return an updated element tree.
+
+    If the namespaces in a document define multiple tags with the same
+    local tag name, this will create ambiguity and lead to errors. Most
+    XML documents, however, only actively use one namespace."""
     for elem in el.iter():
         elem.tag = etree.QName(elem).localname
     etree.cleanup_namespaces(el)
