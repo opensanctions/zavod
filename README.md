dad6f8c77ae8a705820e1a7f44b2088440ebcbe9
Friedrich Lindenberg <friedrich@pudo.org>
2022-06-11 19:56:45 +0200

    initial commit with lib skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# zavod
+
+Zavod is the FollowTheMoney data factory. It contains a variety of useful functions for building small and reproducible data pipelines that generate FtM graphs.
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+
+setup(
+    name="zavod",
+    version="0.1.0",
+    description="Data factory for followthemoney data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="data mapping identity followthemoney etl parsing",
+    author="Friedrich Lindenberg",
+    author_email="friedrich@pudo.org",
+    url="https://github.com/opensanctions/zavod",
+    license="MIT",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    namespace_packages=[],
+    include_package_data=True,
+    package_data={"": ["zavod/data/*", "zavod/py.typed"]},
+    zip_safe=False,
+    install_requires=[
+        "followthemoney >= 2.9.4, < 3.0.0",
+        "requests >= 10.9.0, < 13.0.0",
+        "click >= 8.0.0, < 8.1.0",
+    ],
+    tests_require=[],
+    entry_points={
+        "console_scripts": [],
+    },
+    extras_require={
+        "dev": [
+            "wheel>=0.29.0",
+            "twine",
+            "mypy",
+            "flake8>=2.6.0",
+            "pytest",
+            "pytest-cov",
+            "coverage>=4.1",
+            "types-setuptools",
+            "types-requests",
+        ]
+    },
+)
