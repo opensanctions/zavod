989775c10aa67733c1ad6f59b518abfa58a1c4dc
Friedrich Lindenberg <friedrich@pudo.org>
2022-06-15 11:48:29 +0200

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="zavod",
-    version="0.1.2",
+    version="0.2.0",
     description="Data factory for followthemoney data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
