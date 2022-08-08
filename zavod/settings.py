6b83d3a1f14c1fd5fa9f7c96fd536f886ef984af
Friedrich Lindenberg <friedrich@pudo.org>
2022-08-08 15:14:35 +0200

    Changes

--- zavod/settings.py
+++ zavod/settings.py
@@ -5,4 +5,5 @@ DATA_PATH_ = os.environ.get("ZAVOD_DATA_PATH", "data")
 DATA_PATH = Path(DATA_PATH_)
 
 HTTP_TIMEOUT = 1200
-HTTP_USER_AGENT = "Mozilla/5.0 (zavod/ftm-datafactory)"
+HTTP_USER_AGENT = "Mozilla/5.0 (zavod)"
+HTTP_USER_AGENT = os.environ.get("ZAVOD_HTTP_USER_AGENT", HTTP_USER_AGENT)
