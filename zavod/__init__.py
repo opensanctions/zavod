b6c107a04a48231562d16a7005c518425469e89f
Friedrich Lindenberg <friedrich@pudo.org>
2022-08-08 15:26:28 +0200

    Bump version: 0.4.0 → 0.4.1

--- zavod/__init__.py
+++ zavod/__init__.py
@@ -11,7 +11,7 @@ from zavod.logs import configure_logging, get_logger
 from zavod.sinks.common import Sink
 from zavod.sinks.file import JSONFileSink
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __all__ = [
     "init",
     "context",
