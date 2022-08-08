ba7d659df9e41d3f4543e634c28094589568b650
Friedrich Lindenberg <friedrich@pudo.org>
2022-08-08 15:13:54 +0200

    Import name generation from opensanctions

--- zavod/parse/__init__.py
+++ zavod/parse/__init__.py
@@ -1,7 +1,10 @@
 from zavod.parse.addresses import make_address
 from zavod.parse.xml import remove_namespace
+from zavod.parse.names import make_name, apply_name
 
 __all__ = [
     "make_address",
     "remove_namespace",
+    "make_name",
+    "apply_name"
 ]
