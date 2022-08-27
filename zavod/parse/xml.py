e1c176851b1010603b55c700a9faad6961b5aa87
Friedrich Lindenberg <friedrich@pudo.org>
2022-08-27 12:32:32 +0200

    fix up attrs in remove_namespace

--- zavod/parse/xml.py
+++ zavod/parse/xml.py
@@ -1,7 +1,10 @@
+from typing import Union
 from lxml import etree
 
+ElementOrTree = Union[etree._Element, etree._ElementTree]
 
-def remove_namespace(el: etree._Element) -> etree._Element:
+
+def remove_namespace(el: ElementOrTree) -> ElementOrTree:
     """Remove namespace in the passed XML/HTML document in place and
     return an updated element tree.
 
@@ -10,5 +13,9 @@ def remove_namespace(el: etree._Element) -> etree._Element:
     XML documents, however, only actively use one namespace."""
     for elem in el.iter():
         elem.tag = etree.QName(elem).localname
+        for key, value in list(elem.attrib.items()):
+            local_key = etree.QName(key).localname
+            if key != local_key:
+                elem.attrib[local_key] = value
     etree.cleanup_namespaces(el)
     return el
